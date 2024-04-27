943ee9bef67624777f7ec46351138e49fb20a204
muhammadramadhani <rmarmaali@gmail.com>
2024-04-27 13:15:35 +0800

    Operasi String

---
+++ OperasiString.java
@@ -0,0 +1,20 @@
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Banjarbaru";
+        System.out.println(kota);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        //NamaClass namaObject = new Constructor()
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+        System.out.println(uniskaString.toUpperCase());
+        System.out.println(kota.toUpperCase());
+
+        System.out.println(uniskaString.toLowerCase());
+        System.out.println(kota.toLowerCase());
+
+        System.out.println(uniskaString.substring(5));
+        System.out.println(kota.substring(5));
+
+    }
+}
